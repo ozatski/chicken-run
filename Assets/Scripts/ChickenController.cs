6be6cb8070a0f8b34d555a5e70ebc32613caf40c
Thomaz Ozatski <thomaz.ozatski@conestogac.on.ca>
2023-10-26 19:38:09 -0400

    Add scenario

--- Assets/Scripts/ChickenController.cs
+++ Assets/Scripts/ChickenController.cs
@@ -8,18 +8,40 @@ public class ChickenController : MonoBehaviour
     public Transform player;
     private NavMeshAgent agent;
     public float fleeDistance = 5.0f; // Distance at which the chicken starts to flee
+    public float wanderRadius = 10.0f; // for random destinations
+    public Animator animator;
+    private Vector3 randomDestination;
+    private bool isMoving = false;
+    public float wanderInterval = 5.0f; // Time between wander movements
+
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();   
+        agent = GetComponent<NavMeshAgent>();
+        SetRandomDestination();
+        StartCoroutine(Wander());
     }
 
+
+
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Vector3 directionToPlayer = transform.position - player.position;
         float distanceToPlayer = directionToPlayer.magnitude;
+
+        // Check if the agent is moving
+        if (agent.velocity.sqrMagnitude > 0.1f)
+        {
+            // Agent is moving, set the "Run" parameter to true
+            animator.SetBool("Run", true);
+        }
+        else
+        {
+            // Agent is not moving, set the "Run" parameter to false (assuming you have an "Idle" animation state)
+            animator.SetBool("Run", false);
+        }
 
         if (distanceToPlayer < fleeDistance)
         {
@@ -29,5 +51,44 @@ public class ChickenController : MonoBehaviour
             // Set the agent's destination to the flee point
             agent.SetDestination(fleeDestination);
         }
+
+      
+
+
+
+        if (isMoving)
+        {
+            // Check if the chicken has reached its random destination
+            if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+            {
+                isMoving = false;
+            }
+        }
+
+
     }
+
+    void SetRandomDestination()
+    {
+        // Generate a random point within the specified radius
+        Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
+        randomDirection += transform.position;
+        NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, wanderRadius, 1);
+        randomDestination = hit.position;
+    }
+
+    IEnumerator Wander()
+    {
+        while (true)
+        {
+            if (!isMoving)
+            {
+                SetRandomDestination();
+                agent.SetDestination(randomDestination);
+                isMoving = true;
+            }
+            yield return new WaitForSeconds(wanderInterval);
+        }
+    }
+
 }
